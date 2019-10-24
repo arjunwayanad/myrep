c20811b4fb1179bc2623fc5890ac0b079e5b0342
arjun <arjunanil564@gmail.com>
2019-10-24 11:19:53 -0700

    wsdedfef

---
+++ DatabaseFirstMVC/WebApplication4/Controllers/Model1Controller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class Model1Controller : Controller
+    {
+        DB_09_10Entities db = new DB_09_10Entities();
+        // GET: Model1
+        public ActionResult DisplayDonors()
+        {
+
+            var l = db.tblDonors.Where(x => x.City == "Mumbai").ToList();
+            //var l = from d in db.tblDonors
+            //         where d.City == "Mumbai"
+            //         select d;
+            return View(l);
+        }
+
+    }
+}
